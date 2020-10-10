dd91e47558e7649f9af5f2d23e43eebd6533ded0
Suraj <surajjadhav9741@gmail.com>
2020-10-10 21:49:20 +0530

    Modification : Solving using switch case

--- empWage.java
+++ empWage.java
@@ -1,27 +1,31 @@
 import java.util.Random;
 class empMethods{
 	int Wage_per_hour=20;
-	int Day_hour=8;
-	int Part_time=4;
+	int Day_hour=0;
 	int Total_Wage=1;
 	Random rand = new Random();
 	void check(){
 		int choice = rand.nextInt(10) % 3;
-		if(choice==0){
-			System.out.println("Present Full time employee");
-			Total_Wage=Wage_per_hour*Day_hour;
-			System.out.println("Total Wage : "+Total_Wage);
+		switch (choice){
+			case 0:
+				System.out.println("Present Full time employee");
+				Day_hour=8;
+				break;
+			case 1:
+				System.out.println("Present Part time employee");
+				Day_hour=4;
+				break;
+			case 2:
+				System.out.println("Absent");
+				Day_hour=0;
+				break;
+			default:
+				System.out.println("Invalid");
+				Day_hour=0;
+				break;
 		}
-		else if(choice==1){
-			System.out.println("Present Part time employee");
-			Total_Wage=Wage_per_hour*Part_time;
-			System.out.println("Total Wage : "+Total_Wage);
-		}
-		else{
-			System.out.println("Absent");
-			Total_Wage=0;
-			System.out.println("Total Wage : "+Total_Wage);
-		}
+		Total_Wage=Wage_per_hour*Day_hour;
+		System.out.println("Total Wage of Day : "+Total_Wage);
 	}
 }
 public class empWage{
@@ -29,6 +33,5 @@ public class empWage{
 		System.out.println("Welcome to Wage Computation Program");
 		empMethods e1=new empMethods();
 		e1.check();
-		//System.out.println(choice + " Hello world");
 	}
 }
