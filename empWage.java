a689db026519d4bb9a2103c151c94479c037278b
Suraj <surajjadhav9741@gmail.com>
2020-10-10 22:05:30 +0530

    Add : Claculate Wage for Month

--- empWage.java
+++ empWage.java
@@ -3,29 +3,37 @@ class empMethods{
 	int Wage_per_hour=20;
 	int Day_hour=0;
 	int Total_Wage=1;
+	int count=1;
 	Random rand = new Random();
 	void check(){
-		int choice = rand.nextInt(10) % 3;
-		switch (choice){
-			case 0:
-				System.out.println("Present Full time employee");
-				Day_hour=8;
-				break;
-			case 1:
-				System.out.println("Present Part time employee");
-				Day_hour=4;
-				break;
-			case 2:
-				System.out.println("Absent");
-				Day_hour=0;
-				break;
-			default:
-				System.out.println("Invalid");
-				Day_hour=0;
-				break;
+		while(count<21){
+			int choice = rand.nextInt(10) % 3;
+			switch (choice){
+				case 0:
+					System.out.println("------------------------------------------------------");
+					System.out.println("Present Full time employee");
+					Day_hour=8;
+					break;
+				case 1:
+					System.out.println("------------------------------------------------------");
+					System.out.println("Present Part time employee");
+					Day_hour=4;
+					break;
+				case 2:
+					System.out.println("------------------------------------------------------");
+					System.out.println("Absent");
+					Day_hour=0;
+					break;
+				default:
+					System.out.println("------------------------------------------------------");
+					System.out.println("Invalid");
+					Day_hour=0;
+					break;
+			}
+			Total_Wage=Wage_per_hour*Day_hour;
+			System.out.println("Total Wage of "+count+" Day : "+Total_Wage);
+			count=count+1;
 		}
-		Total_Wage=Wage_per_hour*Day_hour;
-		System.out.println("Total Wage of Day : "+Total_Wage);
 	}
 }
 public class empWage{
