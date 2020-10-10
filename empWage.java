aabd39c0dc8f0aeb3738353c88613b711d77d6c5
Suraj <surajjadhav9741@gmail.com>
2020-10-10 21:35:22 +0530

    Add : Part time wage

--- empWage.java
+++ empWage.java
@@ -2,24 +2,33 @@ import java.util.Random;
 class empMethods{
 	int Wage_per_hour=20;
 	int Day_hour=8;
+	int Part_time=4;
 	int Total_Wage=1;
+	Random rand = new Random();
+	void check(){
+		int choice = rand.nextInt(10) % 3;
+		if(choice==0){
+			System.out.println("Present Full time employee");
+			Total_Wage=Wage_per_hour*Day_hour;
+			System.out.println("Total Wage : "+Total_Wage);
+		}
+		else if(choice==1){
+			System.out.println("Present Part time employee");
+			Total_Wage=Wage_per_hour*Part_time;
+			System.out.println("Total Wage : "+Total_Wage);
+		}
+		else{
+			System.out.println("Absent");
+			Total_Wage=0;
+			System.out.println("Total Wage : "+Total_Wage);
+		}
+	}
 }
 public class empWage{
 	public static void main(String args[]){
 		System.out.println("Welcome to Wage Computation Program");
-		Random rand = new Random();
 		empMethods e1=new empMethods();
-		int choice = rand.nextInt(10) % 2;
-		if(choice==0){
-			System.out.println("Present");
-			e1.Total_Wage=e1.Wage_per_hour*e1.Day_hour;
-			System.out.println("Total Wage : "+e1.Total_Wage);
-		}
-		else{
-			System.out.println("Absent");
-			e1.Total_Wage=0;
-			System.out.println("Total Wage : "+e1.Total_Wage);
-		}
+		e1.check();
 		//System.out.println(choice + " Hello world");
 	}
 }
