5fcca162b25213670dd4835c9977db4152e6bd43
Suraj <surajjadhav9741@gmail.com>
2020-10-10 22:23:18 +0530

    Add : Calculate Wage till a condition of total working hours or days of working is completed

--- empWage.java
+++ empWage.java
@@ -4,9 +4,11 @@ class empMethods{
 	int Day_hour=0;
 	int Total_Wage=1;
 	int count=1;
+	int hours=0;
+	int Total_Month_Wage=0;
 	Random rand = new Random();
 	void check(){
-		while(count<21){
+		while(count<21&&hours<100){
 			int choice = rand.nextInt(10) % 3;
 			switch (choice){
 				case 0:
@@ -30,10 +32,17 @@ class empMethods{
 					Day_hour=0;
 					break;
 			}
+			hours=hours+Day_hour;
 			Total_Wage=Wage_per_hour*Day_hour;
+			Total_Month_Wage=Total_Month_Wage+Total_Wage;
 			System.out.println("Total Wage of "+count+" Day : "+Total_Wage);
 			count=count+1;
 		}
+		System.out.println("------------------------------------------------------");
+		System.out.println("------------------------Total-------------------------");
+		System.out.println("Total Month Wage : "+Total_Month_Wage);
+		System.out.println("Total Month Work Hours : "+hours);
+		System.out.println("------------------------------------------------------");
 	}
 }
 public class empWage{
