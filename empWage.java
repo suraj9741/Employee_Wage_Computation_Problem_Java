de9e02511613551ced12d04fe2c2f1c7712f4267
Suraj <surajjadhav9741@gmail.com>
2020-10-10 19:03:49 +0530

    Add : Empwage java file

---
+++ empWage.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+public class empWage{
+	public static void main(String args[]){
+		System.out.println("Welcome to Wage Computation Program");
+		Random rand = new Random();
+		int choice = rand.nextInt(10) % 2;
+		if(choice==0)
+			System.out.println("Present");
+		else
+			System.out.println("Absent");
+		//System.out.println(choice + " Hello world");
+	}
+}
