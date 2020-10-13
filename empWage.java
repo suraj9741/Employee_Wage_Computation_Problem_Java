afc95d291657c2814750a9f128720f7917025a74
Suraj <surajjadhav9741@gmail.com>
2020-10-13 22:24:53 +0530

    Modified : class name in UC-4

--- empWage.java
+++ empWage.java
@@ -1,5 +1,5 @@
 import java.util.Random;
-class empMethods{
+class EmpMethods{
 	int Wage_per_hour=20;
 	int Day_hour=0;
 	int Total_Wage=1;
@@ -28,10 +28,10 @@ class empMethods{
 		System.out.println("Total Wage of Day : "+Total_Wage);
 	}
 }
-public class empWage{
+public class EmpWage{
 	public static void main(String args[]){
 		System.out.println("Welcome to Wage Computation Program");
-		empMethods e1=new empMethods();
-		e1.check();
+		EmpMethods call=new EmpMethods();
+		call.check();
 	}
 }
