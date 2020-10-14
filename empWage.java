c7b17513a0132c5890026ff50cc6d31659cc631d
Suraj <surajjadhav9741@gmail.com>
2020-10-15 00:23:56 +0530

    Add : Ability to manage Multipale company by using function

--- empWage.java
+++ empWage.java
@@ -5,10 +5,13 @@ class EmpMethods{
 	int Total_Woking_Days=1;
 	int hours=0;
 	
-	private final String Company;
-	private final int Emp_Rate_Per_Hour;
-	private final int Num_Of_Working_Days;
-	private final int Max_Hours_Per_Month;
+	public int TotalEmpWage;
+	
+	
+	public final String Company;
+	public final int Emp_Rate_Per_Hour;
+	public final int Num_Of_Working_Days;
+	public final int Max_Hours_Per_Month;
 	
 	public EmpMethods(String Company, int Emp_Rate_Per_Hour, int Num_Of_Working_Days, int Max_Hours_Per_Month){
 		this.Company=Company;
@@ -17,44 +20,74 @@ class EmpMethods{
 		this.Num_Of_Working_Days=Num_Of_Working_Days;
 	}
 	
+	public void setTotalEmpWage(int TotalEmpWage){
+		this.TotalEmpWage=TotalEmpWage;
+	}
+	//public String toString(){	return 0 }
+	
+	
+	
 	int Total_Month_Wage=0;
-	Random rand = new Random();
-	void check(){
-		while(Total_Woking_Days<=Num_Of_Working_Days&&hours<Max_Hours_Per_Month){
+	
+}
+public class EmpWage{
+	
+		Random rand = new Random();
+		
+		private EmpMethods[] companyEmpArray;
+		private int NumOfCompany=0;
+		
+		
+		public EmpWage(){
+			companyEmpArray =new EmpMethods[5];
+		}
+		
+		private void addCompnyEmpWage(String Company, int Emp_Rate_Per_Hour, int Num_Of_Working_Days, int Max_Hours_Per_Month){
+			companyEmpArray[NumOfCompany]=new EmpMethods(Company,Emp_Rate_Per_Hour,Num_Of_Working_Days,Max_Hours_Per_Month);
+			NumOfCompany++;
+		}
+		
+		private void computeEmpWage(){
+			for (int i=0;i<NumOfCompany;i++){
+				companyEmpArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpArray[i]));
+				//System.out.println(companyEmpArray[i]);
+			}
+		}
+		private int computeEmpWage(EmpMethods companyEmpWage){
+			while(companyEmpWage.Total_Woking_Days<=companyEmpWage.Num_Of_Working_Days&&companyEmpWage.hours<companyEmpWage.Max_Hours_Per_Month){
 			int choice = rand.nextInt(10) % 3;
 			switch (choice){
 				case 0:
-					Day_hour=8;
+					companyEmpWage.Day_hour=8;
 					break;
 				case 1:
-					Day_hour=4;
+					companyEmpWage.Day_hour=4;
 					break;
 				case 2:
-					Day_hour=0;
+					companyEmpWage.Day_hour=0;
 					break;
 				default:
-					Day_hour=0;
+					companyEmpWage.Day_hour=0;
 					break;
 			}
-			hours=hours+Day_hour;
-			Total_Wage=Emp_Rate_Per_Hour*Day_hour;
-			Total_Month_Wage=Total_Month_Wage+Total_Wage;
-			Total_Woking_Days=Total_Woking_Days+1;
+			companyEmpWage.hours=companyEmpWage.hours+companyEmpWage.Day_hour;
+			companyEmpWage.Total_Wage=companyEmpWage.Emp_Rate_Per_Hour*companyEmpWage.Day_hour;
+			companyEmpWage.Total_Month_Wage=companyEmpWage.Total_Month_Wage+companyEmpWage.Total_Wage;
+			companyEmpWage.Total_Woking_Days=companyEmpWage.Total_Woking_Days+1;
 		}
 		System.out.println("------------------------Total-------------------------");
-		System.out.println("Total Month Wage in "+ Company +" is : "+Total_Month_Wage);
-		System.out.println("Total Month Work Hours in "+ Company +" is : "+hours+" Out of "+Max_Hours_Per_Month);
-		System.out.println("Total Month Working Days in "+ Company +" is : "+(Total_Woking_Days-1)+" Out of "+Num_Of_Working_Days);
+		System.out.println("Total Month Wage in "+ companyEmpWage.Company +" is : "+companyEmpWage.Total_Month_Wage);
+		System.out.println("Total Month Work Hours in "+ companyEmpWage.Company +" is : "+companyEmpWage.hours+" Out of "+companyEmpWage.Max_Hours_Per_Month);
+		System.out.println("Total Month Working Days in "+ companyEmpWage.Company +" is : "+(companyEmpWage.Total_Woking_Days-1)+" Out of "+companyEmpWage.Num_Of_Working_Days);
 		System.out.println("------------------------------------------------------");
-	}
-}
-public class EmpWage{
-	public static void main(String args[]){
-		System.out.println("Welcome to Wage Computation Program");
-		EmpMethods Dmart=new EmpMethods("Dmart", 20, 20, 100 );
-		Dmart.check();
-		System.out.println("#################################Next Company#########################################");
-		EmpMethods Reliance=new EmpMethods("Reliance", 10, 25, 120);
-		Reliance.check();
+		return companyEmpWage.Total_Month_Wage;		
+		}
+		
+		
+		public static void main(String args[]){
+		EmpWage EmpWageBuilder = new EmpWage();
+		EmpWageBuilder.addCompnyEmpWage("Dmart", 20, 20, 100);
+		EmpWageBuilder.addCompnyEmpWage("Reliance", 10, 25, 120);
+		EmpWageBuilder.computeEmpWage();
 	}
 }
